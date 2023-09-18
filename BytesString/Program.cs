c953494df2be6d0ffa945512b939d16b6101f113
Gideon Isa <innocentgideon10@gmail.com>
2023-09-18 22:04:46 +0100

    second function

---
+++ BytesString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace BytesString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            byte[] buffer = new byte[4];
+
+            //string myName = "Gid";
+            //buffer = Encoding.ASCII.GetBytes(myName);
+
+            //byte[] buffer = new byte[4];
+            buffer[0] = 0;
+            buffer[1] = 56;
+            foreach (var item in buffer)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Console.WriteLine(buffer.ToString);
+            
+
+
+        }
+    }
+}
